eb1c24cfd986c3b14dfe6364f02b6cbdc9a91aa7
DVDAndroid <davide.intelligent@gmail.com>
2015-02-03 14:36:34 +0100

    updated app

--- app/src/main/java/com/dvd/android/example/htmlcolordecoder/MainActivity.java
+++ app/src/main/java/com/dvd/android/example/htmlcolordecoder/MainActivity.java
@@ -28,10 +28,12 @@ public class MainActivity extends Activity {
 			public void onTextChanged(CharSequence s, int start, int before,
 					int count) {
 				if (et.getText().toString() != null) {
+                    et.setTextColor(getResources().getColor(android.R.color.holo_green_dark));
 					try {
 						iv.setBackgroundColor(Color.parseColor("#" + et.getText()
 								.toString()));
 					} catch (IllegalArgumentException e) {
+                        et.setTextColor(getResources().getColor(android.R.color.holo_red_dark));
 						iv.setBackgroundColor(Color.parseColor("#ffffff"));
 					}
 				} else {
